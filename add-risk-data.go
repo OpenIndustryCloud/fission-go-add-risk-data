7689e1ea93c5a195558d69c2c6707235b4408aad
Amit Varman <AmitKumarVarman@gmail.com>
2017-11-13 00:26:25 +0000

    resonse status added

--- add-risk-data.go
+++ add-risk-data.go
@@ -28,45 +28,55 @@ const (
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 
-	if r.Body == nil {
-		http.Error(w, "Please send a valid JSON", 400)
-		return
-	}
-	var composedResult ComposedResult
+	var composedResult = ComposedResult{}
 	err := json.NewDecoder(r.Body).Decode(&composedResult)
 	if err != nil {
-		http.Error(w, err.Error(), 400)
+		createErrorResponse(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	// populate ticket payload with custom field
-	ticketDetails := composedResult.TranformedData.TicketDetails
-	var claimType = ""
-	if strings.Contains(ticketDetails.Ticket.Subject, "Storm") {
-		claimType = "Storm Surge"
-		ticketDetails.Ticket.TicketFormID = STORM_FORM_ID
-	} else {
-		claimType = "TV"
-		ticketDetails.Ticket.TicketFormID = TV_FORM_ID
+	ticketDetails := TicketDetails{}
+	var claimType, riskDesc, windSpeed, tvModel string
+	if composedResult.TranformedData.Status == 200 {
+		ticketDetails = composedResult.TranformedData.TicketDetails
+		if strings.Contains(ticketDetails.Ticket.Subject, "Storm") {
+			claimType = "Storm Surge"
+			ticketDetails.Ticket.TicketFormID = STORM_FORM_ID
+		} else {
+			claimType = "TV"
+			ticketDetails.Ticket.TicketFormID = TV_FORM_ID
+		}
 	}
-	weatherRisk := composedResult.WeatherRisk
-	weatherData := composedResult.WeatherData
-	riskDesc := strconv.Itoa(weatherRisk.RiskScore) + " : " + weatherRisk.Description
+
+	if composedResult.WeatherRisk.Status == 200 {
+		weatherRisk := composedResult.WeatherRisk
+		riskDesc = strconv.Itoa(weatherRisk.RiskScore) + " : " + weatherRisk.Description
+	}
+	if composedResult.WeatherData.Status == 200 {
+		weatherData := composedResult.WeatherData
+		windSpeed = weatherData.History.Dailysummary[0].Maxwspdm
+	}
+	if (TVClaimData{}) != composedResult.TranformedData.TVClaimData {
+		tvModel = composedResult.TranformedData.TVClaimData.TVModelNo
+	}
 
 	customFields := []CustomFields{
-		CustomFields{WIND_SPEED_FIELD_ID, weatherData.History.Dailysummary[0].Maxwspdm},
+		CustomFields{WIND_SPEED_FIELD_ID, windSpeed},
 		CustomFields{CLAIM_TYPE_FIELD_ID, claimType},
 		CustomFields{WEATHER_RISK_FIELD_ID, riskDesc},
-		//CustomFields{FRAUD_RISK_FIELD_ID, ""},
-		CustomFields{PHONE_FIELD_ID, ticketDetails.Ticket.Phone},
-		CustomFields{EMAIL_FIELD_ID, ticketDetails.Ticket.Email}}
+		CustomFields{PHONE_FIELD_ID, ticketDetails.Ticket.Requester.Phone},
+		CustomFields{EMAIL_FIELD_ID, ticketDetails.Ticket.Requester.Email},
+		CustomFields{TV_MODEL_FIELD_ID, tvModel},
+	}
 
 	ticketDetails.Ticket.CustomFields = customFields
+	ticketDetails.Status = 200
 
 	//marshal to JSON
 	ticketDetailsJSON, err := json.Marshal(ticketDetails)
 	if err != nil {
-		http.Error(w, err.Error(), 400)
+		createErrorResponse(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
@@ -75,22 +85,39 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func main() {
-	println("staritng app..")
-	http.HandleFunc("/", Handler)
-	http.ListenAndServe(":8084", nil)
+// func main() {
+// 	println("staritng app..")
+// 	http.HandleFunc("/", Handler)
+// 	http.ListenAndServe(":8084", nil)
+// }
+
+func createErrorResponse(w http.ResponseWriter, message string, status int) {
+	errorJSON, _ := json.Marshal(&Error{
+		Status:  status,
+		Message: message})
+	//Send custom error message to caller
+	w.WriteHeader(status)
+	w.Header().Set("content-type", "application/json")
+	w.Write([]byte(errorJSON))
+}
+
+type Error struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
 }
 
 type ComposedResult struct {
-	TranformedData TranformedData `json:"tranformedData"`
-	WeatherData    WeatherData    `json:"weatherData"`
+	TranformedData TranformedData `json:"tranformed-data"`
+	WeatherData    WeatherData    `json:"weather-data"`
 	WeatherRisk    struct {
+		Status      int    `json:"status"`
 		Description string `json:"description"`
 		RiskScore   int    `json:"riskScore"`
-	} `json:"weatherRisk"`
+	} `json:"weather-risk"`
 }
 
 type WeatherData struct {
+	Status  int `json:"status"`
 	History struct {
 		Dailysummary []struct {
 			Fog          string `json:"fog"`
@@ -114,29 +141,60 @@ type CustomFields struct {
 }
 
 type TranformedData struct {
-	TicketDetails struct {
-		Ticket struct {
-			Comment struct {
-				HTMLBody string `json:"html_body"`
-			} `json:"comment"`
-			CustomFields []CustomFields `json:"custom_fields"`
-			Requester    struct {
-				LocaleID int    `json:"locale_id"`
-				Name     string `json:"name"`
-				Email    string `json:"email"`
-			} `json:"requester"`
+	Status          int             `json:"status,omitempty"`
+	TicketDetails   TicketDetails   `json:"ticket_details,omitempty"`
+	WeatherAPIInput WeatherAPIInput `json:"weather_api_input,omitempty"`
+	TVClaimData     TVClaimData     `json:"tv_claim_data,omitempty"`
+	StromClaimData  StromClaimData  `json:"storm_claim_data,omitempty"`
+}
+
+type TVClaimData struct {
+	TVPrice         string `json:"tv_price,omitempty"`
+	CrimeRef        string `json:"crime_ref,omitempty"`
+	IncidentDate    string `json:"incident_date,omitempty"`
+	TVModelNo       string `json:"tv_model_no,omitempty"`
+	TVMake          string `json:"tv_make,omitempty"`
+	TVSerialNo      string `json:"tv_serial_no,omitempty"`
+	DamageImageURL1 string `json:"damage_image_url_1,omitempty"`
+	DamageImageURL2 string `json:"damage_image_url_2,omitempty"`
+	TVReceiptImage  string `json:"tv_reciept_image_url"`
+	DamageVideoURL  string `json:"damage_video_url,omitempty"`
+}
+
+type StromClaimData struct {
+	IncidentPlace       string `json:"incident_place,omitempty"`
+	IncidentDate        string `json:"incident_date,omitempty"`
+	DamageImageURL1     string `json:"damage_image_url_1,omitempty"`
+	DamageImageURL2     string `json:"damage_image_url_2,omitempty"`
+	RepairEstimateImage string `json:"estimate_image_url,omitempty"`
+	DamageVideoURL      string `json:"damage_video_url,omitempty"`
+}
+
+type TicketDetails struct {
+	Status int `json:"status"`
+	Ticket struct {
+		Type     string `json:"type"`
+		Subject  string `json:"subject"`
+		Priority string `json:"priority"`
+		Status   string `json:"status"`
+		Comment  struct {
+			HTMLBody string   `json:"html_body"`
+			Uploads  []string `json:"uploads,omitempty"`
+		} `json:"comment"`
+		CustomFields []CustomFields `json:"custom_fields,omitempty"`
+		Requester    struct {
+			LocaleID     int    `json:"locale_id"`
+			Name         string `json:"name"`
 			Email        string `json:"email"`
 			Phone        string `json:"phone"`
-			Priority     string `json:"priority"`
-			Status       string `json:"status"`
-			Subject      string `json:"subject"`
-			Type         string `json:"type"`
-			TicketFormID int64  `json:"ticket_form_id"`
-		} `json:"ticket"`
-	} `json:"ticketDetails"`
-	WeatherAPIInput struct {
-		City    string `json:"city"`
-		Country string `json:"country"`
-		Date    string `json:"date"`
-	} `json:"weatherAPIInput"`
+			PolicyNumber string `json:"policy_number"`
+		} `json:"requester"`
+		TicketFormID int64 `json:"ticket_form_id"`
+	} `json:"ticket"`
+}
+
+type WeatherAPIInput struct {
+	City    string `json:"city,omitempty"`
+	Country string `json:"country,omitempty"`
+	Date    string `json:"date,omitempty"` //YYYYMMDD
 }
