957c341727e959b21afa95250d44a7e3b4ec8901
Amit Varman <AmitKumarVarman@gmail.com>
2017-11-22 13:46:06 +0000

    audit data added

--- add-risk-data.go
+++ add-risk-data.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const (
@@ -17,16 +18,23 @@ const (
 	STORM_FORM_ID = 114093996871
 	TV_FORM_ID    = 114093998312
 	//FIELDS
-	WIND_SPEED_FIELD_ID   = 114100596852
-	TV_MODEL_FIELD_ID     = 114099896612
-	CLAIM_TYPE_FIELD_ID   = 114099964311 // TV , Storm Surge
-	WEATHER_RISK_FIELD_ID = 114100712171
-	FRAUD_RISK_FIELD_ID   = 114100657672
-	PHONE_FIELD_ID        = 114100658992
-	EMAIL_FIELD_ID        = 114100659172
+	WIND_SPEED_FIELD_ID      = 114100596852
+	TV_MODEL_FIELD_ID        = 114099896612
+	CLAIM_TYPE_FIELD_ID      = 114099964311 // TV , Storm Surge
+	WEATHER_RISK_FIELD_ID    = 114100712171
+	FRAUD_RISK_FIELD_ID      = 114100657672
+	FRAUD_CHECK_REQ_FIELD_ID = 114101778331
+
+	PHONE_FIELD_ID         = 114100658992
+	EMAIL_FIELD_ID         = 114100659172
+	DAMAGE_IMAGE_URL_1     = 114101729171
+	DAMAGE_IMAGE_URL_2     = 114101729171
+	RECEIPT_OR_EST_IMG_URL = 114101729331
+	DAMAGE_VIDEO_URL       = 114101655952
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	mediaFields := [4]int64{114101729171, 114101729331, 114101655952, 114101729171}
 
 	var composedResult = ComposedResult{}
 	err := json.NewDecoder(r.Body).Decode(&composedResult)
@@ -37,7 +45,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	// populate ticket payload with custom field
 	ticketDetails := TicketDetails{}
-	var claimType, riskDesc, windSpeed, tvModel string
+	var claimType, riskDesc, windSpeed string
 	if composedResult.TranformedData.Status == 200 {
 		ticketDetails = composedResult.TranformedData.TicketDetails
 		if strings.Contains(ticketDetails.Ticket.Subject, "Storm") {
@@ -47,30 +55,72 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			claimType = "TV"
 			ticketDetails.Ticket.TicketFormID = TV_FORM_ID
 		}
+	} else {
+		createErrorResponse(w, "Ticket Details not recieved", http.StatusBadRequest)
 	}
 
 	if composedResult.WeatherRisk.Status == 200 {
 		weatherRisk := composedResult.WeatherRisk
 		riskDesc = strconv.Itoa(weatherRisk.RiskScore) + " : " + weatherRisk.Description
+		ticketDetails.addCustomFields(CustomFields{WEATHER_RISK_FIELD_ID, riskDesc})
+		println("riskDesc", riskDesc)
+		if weatherRisk.RiskScore <= 20 {
+			ticketDetails.addCustomFields(CustomFields{FRAUD_CHECK_REQ_FIELD_ID, "true"})
+			ticketDetails.addCustomFields(CustomFields{FRAUD_RISK_FIELD_ID, "HIGH"})
+		} else if weatherRisk.RiskScore > 20 && weatherRisk.RiskScore <= 60 {
+			ticketDetails.addCustomFields(CustomFields{FRAUD_CHECK_REQ_FIELD_ID, "true"})
+			ticketDetails.addCustomFields(CustomFields{FRAUD_RISK_FIELD_ID, "MEDIUM"})
+		} else {
+			ticketDetails.addCustomFields(CustomFields{FRAUD_CHECK_REQ_FIELD_ID, "false"})
+			ticketDetails.addCustomFields(CustomFields{FRAUD_RISK_FIELD_ID, "LOW"})
+		}
 	}
 	if composedResult.WeatherData.Status == 200 {
 		weatherData := composedResult.WeatherData
 		windSpeed = weatherData.History.Dailysummary[0].Maxwspdm
+		ticketDetails.addCustomFields(CustomFields{WIND_SPEED_FIELD_ID, windSpeed})
+		println("windSpeed", windSpeed)
 	}
+
+	//add Custom Fields
+	//common fields
+	ticketDetails.addCustomFields(CustomFields{CLAIM_TYPE_FIELD_ID, claimType})
+	ticketDetails.addCustomFields(CustomFields{PHONE_FIELD_ID, ticketDetails.Ticket.Requester.Phone})
+	ticketDetails.addCustomFields(CustomFields{EMAIL_FIELD_ID, ticketDetails.Ticket.Requester.Email})
+
+	//if images stored in Bucket
+	mediaBucket := composedResult.MediaBucket
+	if mediaBucket.Status == 200 {
+		for i, media := range mediaBucket.Media {
+			ticketDetails.addCustomFields(CustomFields{mediaFields[i], media.MediaLink})
+		}
+	}
+
 	if (TVClaimData{}) != composedResult.TranformedData.TVClaimData {
-		tvModel = composedResult.TranformedData.TVClaimData.TVModelNo
+		tvClaimData := composedResult.TranformedData.TVClaimData
+		mediaBucket := composedResult.MediaBucket
+		//TV Field
+		ticketDetails.addCustomFields(CustomFields{TV_MODEL_FIELD_ID, tvClaimData.TVModelNo})
+		if mediaBucket.Status != 200 {
+			//typeform media files
+			ticketDetails.addCustomFields(CustomFields{DAMAGE_IMAGE_URL_1, tvClaimData.DamageImageURL1})
+			ticketDetails.addCustomFields(CustomFields{DAMAGE_IMAGE_URL_2, tvClaimData.DamageImageURL2})
+			ticketDetails.addCustomFields(CustomFields{RECEIPT_OR_EST_IMG_URL, tvClaimData.TVReceiptImage})
+			ticketDetails.addCustomFields(CustomFields{DAMAGE_VIDEO_URL, tvClaimData.DamageVideoURL})
+		}
+	}
+	if (StromClaimData{}) != composedResult.TranformedData.StromClaimData {
+		stormClaimData := composedResult.TranformedData.StromClaimData
+		//Storm Fields
+		if mediaBucket.Status != 200 {
+			ticketDetails.addCustomFields(CustomFields{DAMAGE_IMAGE_URL_1, stormClaimData.DamageImageURL1})
+			ticketDetails.addCustomFields(CustomFields{DAMAGE_IMAGE_URL_2, stormClaimData.DamageImageURL2})
+			ticketDetails.addCustomFields(CustomFields{RECEIPT_OR_EST_IMG_URL, stormClaimData.RepairEstimateImage})
+			ticketDetails.addCustomFields(CustomFields{DAMAGE_VIDEO_URL, stormClaimData.DamageVideoURL})
+		}
 	}
 
-	customFields := []CustomFields{
-		CustomFields{WIND_SPEED_FIELD_ID, windSpeed},
-		CustomFields{CLAIM_TYPE_FIELD_ID, claimType},
-		CustomFields{WEATHER_RISK_FIELD_ID, riskDesc},
-		CustomFields{PHONE_FIELD_ID, ticketDetails.Ticket.Requester.Phone},
-		CustomFields{EMAIL_FIELD_ID, ticketDetails.Ticket.Requester.Email},
-		CustomFields{TV_MODEL_FIELD_ID, tvModel},
-	}
-
-	ticketDetails.Ticket.CustomFields = customFields
+	//add status 200 if no error
 	ticketDetails.Status = 200
 
 	//marshal to JSON
@@ -85,11 +135,28 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// func main() {
-// 	println("staritng app..")
-// 	http.HandleFunc("/", Handler)
-// 	http.ListenAndServe(":8084", nil)
-// }
+func (ticketDetails *TicketDetails) addCustomFields(customField CustomFields) {
+	ticketDetails.Ticket.CustomFields = append(ticketDetails.Ticket.CustomFields, customField)
+}
+
+type MediaBucket struct {
+	Status int     `json:"status,omitempty"`
+	Media  []Media `json:"media,omitempty"`
+}
+
+type Media struct {
+	Bucket       string `json:"bucket,omitempty"`
+	Name         string `json:"name,omitempty"`
+	Size         int64  `json:"size,omitempty"`
+	MediaLink    string `json:"media-link,omitempty"`
+	OriginalLink string `json:"original-link,omitempty"`
+}
+
+func main() {
+	println("staritng app..")
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":8084", nil)
+}
 
 func createErrorResponse(w http.ResponseWriter, message string, status int) {
 	errorJSON, _ := json.Marshal(&Error{
@@ -107,6 +174,7 @@ type Error struct {
 }
 
 type ComposedResult struct {
+	MediaBucket    MediaBucket    `json:"media-bucket"`
 	TranformedData TranformedData `json:"tranformed-data"`
 	WeatherData    WeatherData    `json:"weather-data"`
 	WeatherRisk    struct {
@@ -189,7 +257,10 @@ type TicketDetails struct {
 			Phone        string `json:"phone"`
 			PolicyNumber string `json:"policy_number"`
 		} `json:"requester"`
-		TicketFormID int64 `json:"ticket_form_id"`
+		TicketFormID int64     `json:"ticket_form_id"`
+		EventID      string    `json:"event_id"`
+		Token        string    `json:"token"`
+		SubmittedAt  time.Time `json:"submitted_at"`
 	} `json:"ticket"`
 }
 
